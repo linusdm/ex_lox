32c060a74225b6e0884cd98944c6fba4baf6be08
Linus De Meyere <linus.dm@gmail.com>
2024-07-02 21:18:43 +0200

    implement String.Chars for Token

--- lib/token.ex
+++ lib/token.ex
@@ -1,3 +1,7 @@
 defmodule ExLox.Token do
   defstruct [:type, :lexeme, :literal, :line]
 end
+
+defimpl String.Chars, for: ExLox.Token do
+  def to_string(token), do: "#{token.type} #{token.lexeme} #{token.literal}"
+end
