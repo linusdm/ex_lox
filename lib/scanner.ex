57fe2434e4f36d0599b247139c13b4c9d615a6e7
Linus De Meyere <linus.dm@gmail.com>
2024-07-09 20:54:47 +0200

    scanner: reorder tuple

--- lib/scanner.ex
+++ lib/scanner.ex
@@ -7,6 +7,7 @@ defmodule ExLox.Scanner do
     {status, Enum.reverse(tokens)}
   end
 
+  defguardp is_whitespace(char) when char in [" ", "\r", "\t"]
   defguardp is_alpha(char) when char in ?a..?z or char in ?A..?Z or char == ?_
   defguardp is_numeric(char) when char in ?0..?9
   defguardp is_alpha_numeric(char) when is_alpha(char) or is_numeric(char)
@@ -46,43 +47,43 @@ defmodule ExLox.Scanner do
     # should be matched first.
     # Maximal munch: when two lexical grammar rules can both maatch a chunk of code that the scanner
     # is looking at, whichever one matches the most characters wins.
-    {rest, status, tokens, line} =
+    {status, tokens, line, rest} =
       case source do
         <<"//", rest::binary>> ->
-          {consume_rest_of_line(rest), status, tokens, line}
+          {status, tokens, line, consume_rest_of_line(rest)}
 
         <<lexeme::binary-size(2), rest::binary>> when lexeme in @two_char_lexemes ->
-          {rest, status, add_token(tokens, @lexemes_to_types[lexeme], lexeme, line), line}
+          {status, add_token(tokens, @lexemes_to_types[lexeme], lexeme, line), line, rest}
 
         <<lexeme::binary-size(1), rest::binary>> when lexeme in @one_char_lexemes ->
-          {rest, status, add_token(tokens, @lexemes_to_types[lexeme], lexeme, line), line}
+          {status, add_token(tokens, @lexemes_to_types[lexeme], lexeme, line), line, rest}
 
-        <<lexeme::binary-size(1), rest::binary>> when lexeme in [" ", "\r", "\t"] ->
-          {rest, status, tokens, line}
+        <<char::binary-size(1), rest::binary>> when is_whitespace(char) ->
+          {status, tokens, line, rest}
 
         <<"\n", rest::binary>> ->
-          {rest, status, tokens, line + 1}
+          {status, tokens, line + 1, rest}
 
         <<"\"", _rest::binary>> = source ->
           case consume_string_literal(source, line) do
             {:ok, lexeme, literal, rest, line} ->
-              {rest, status, add_token(tokens, :string, lexeme, line, literal), line}
+              {status, add_token(tokens, :string, lexeme, line, literal), line, rest}
 
             {:error, rest, line} ->
-              {rest, :error, tokens, line}
+              {:error, tokens, line, rest}
           end
 
         <<char::utf8, _rest::binary>> = source when is_numeric(char) ->
           {lexeme, literal, rest} = consume_number_literal(source)
-          {rest, status, add_token(tokens, :number, lexeme, line, literal), line}
+          {status, add_token(tokens, :number, lexeme, line, literal), line, rest}
 
         <<char::utf8, _rest::binary>> = source when is_alpha(char) ->
           {lexeme, type, rest} = consume_identifier(source)
-          {rest, status, add_token(tokens, type, lexeme, line), line}
+          {status, add_token(tokens, type, lexeme, line), line, rest}
 
         <<_::binary-size(1), rest::binary>> ->
           ExLox.error(line, "Unexpected character.")
-          {rest, :error, tokens, line}
+          {:error, tokens, line, rest}
       end
 
     scan_tokens_recursive(rest, status, tokens, line)
